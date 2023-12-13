5ff951a95bafa6f7ed1231ebd7101b11b7522693
ticketpaper <122894395+ticketpaper@users.noreply.github.com>
2023-12-13 11:27:18 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 7.49 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 0.64 ms
+메모리: 78.4 MB, 시간: 7.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 13:47:51
+2023년 12월 3일 11:27:15
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,31 +1,23 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] numbers) {
-        int index = 0, index2 = 0;
-        Arrays.sort(numbers);
-
-        int[] temp = new int[numbers.length*numbers.length];
-        for (int i = 0; i < numbers.length-1; i++) {
+        List<Integer> combi = new ArrayList<>();
+        for (int i = 0; i < numbers.length; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
-                temp[index]= numbers[i]+numbers[j];
-                index++;
+                combi.add(numbers[i]+numbers[j]);
             }
         }
+        System.out.println(combi);
+        combi.sort(Comparator.naturalOrder());
+        Set<Integer> answer_set = new TreeSet<>(combi);
+        System.out.println(answer_set);
 
-        int[] temp_ans = Arrays.copyOfRange(temp, 0, index);
-        Arrays.sort(temp_ans);
-
-        int[] temp_answer = new int[temp_ans.length];
-        for (int i = 0; i < temp_ans.length - 1; i++) {
-            if (temp_ans[i] != temp_ans[i + 1]) {
-                temp_answer[index2] = temp_ans[i];
-                index2++;
-            }
+        int[] answer = new int[answer_set.size()];
+        int count = 0;
+        for (Integer i : answer_set) {
+            answer[count] = i;
+            count++;
         }
-        temp_answer[index2++] = temp_ans[temp_ans.length - 1];
-        int[] answer = Arrays.copyOfRange(temp_answer, 0, index2);
-
         return answer;
     }
 }
