6ae80cd8b11e745c2590004b7c295fa8a901df4c
ticketpaper <122894395+ticketpaper@users.noreply.github.com>
2023-12-19 15:16:47 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -3,24 +3,21 @@ import java.util.*;
 class Solution {
     static int answer;
     public int solution(int k, int[][] dungeons) {
-        Stack<Integer> stack = new Stack<>();
         boolean[] dnfvisit = new boolean[dungeons.length];
-        Dnf(dungeons, dnfvisit,stack, k,0);
+        Dnf(dungeons, dnfvisit, k,0);
         return answer;
     }
     
-    static void Dnf(int[][] Dungeons, boolean[] visit, Stack<Integer> stack, int k, int countd) {
+    static void Dnf(int[][] Dungeons, boolean[] visit, int k, int countd) {
         for (int i = 0; i < Dungeons.length; i++) {
-            if (stack.size() > answer) {
-                answer= stack.size();
+            if (countd > answer) {
+                answer= countd;
             }
             if (visit[i]==false && k >= Dungeons[i][0]) {
                 visit[i] = true;
-                stack.add(i);
                 k -= Dungeons[i][1];
-                Dnf(Dungeons, visit, stack, k,countd+1);
+                Dnf(Dungeons, visit, k,countd+1);
                 k += Dungeons[i][1];
-                stack.pop();
                 visit[i] = false;
             }
         }
